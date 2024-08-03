afe9d67af8a3778ae693b31526d0e4394ed6e36a
aditya choubisa <adityachoubisa02@gmail.com>
2024-08-03 22:58:52 +0530

    Initial Commit

---
+++ SnackShopApp/SnackShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackShopAppApp.swift
+//  SnackShopApp
+//
+//  Created by APPLE on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SnackShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
